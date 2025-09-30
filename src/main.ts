323607aa1573ee5b3bda660c1e6e6b4fc7502deb
Camila Barpp <milabarpp5@gmail.com>
2025-09-30 10:37:49 -0300

    feat: update CORS configuration to support multiple origins

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,16 @@ import * as bodyParser from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: [
+      'http://localhost:3001',
+      'https://camilabarpp.github.io',
+      'https://camilabarpp.github.io/financial-controll-site',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  app.use(bodyParser.json({ limit: '10mb' })); // ajuste o valor conforme necessário
+  app.use(bodyParser.json({ limit: '10mb' }));
   app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
-  await app.listen(3002);
+  await app.listen(process.env.PORT || 3002);
 }
 bootstrap();

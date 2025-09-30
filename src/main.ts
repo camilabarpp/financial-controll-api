dede52691b85f2ea95c80d0e35770df40df3fddd
Camila Barpp <milabarpp5@gmail.com>
2025-09-30 10:53:36 -0300

    fix: remove localhost from CORS origins

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-      'http://localhost:3001',
       'https://camilabarpp.github.io',
       'https://camilabarpp.github.io/financial-controll-site',
     ],
